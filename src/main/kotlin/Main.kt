652d67d06baad8af7936594106d57e5e5a74bdfd
1101-22 <alya.korobeynikova99@gmail.com>
2023-11-17 13:25:20 +0300

    z12

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Быки и коровы")
+    var chisl_com = enter_com()
+    println(chisl_com)
+    var chisl_pol = enter_pol().chisl_pol
+}
+class enter_pol {
+    var  chisl_pol = ""
+    init {
+        var valid = false
+        do {
+            try {
+                chisl_pol = readLine().toString()
+                chisl_pol.toInt()
+                if (chisl_pol.length == 4)
+                    valid = true
+                else
+                    println("Ошибка. Введите четырехзначное число.")
+            } catch (e: NumberFormatException) {
+                println("Ошибка. Введите целое число.")
+            }
+        } while (!valid)
+    }
+}
+
+val gener_com = mutableSetOf<String>()
+fun enter_com(): String {
+    var  number_com = ""
+    do {
+        number_com = (0 .. 9999).random().toString().padStart(4,'0')
+    } while (number_com in gener_com)
+    gener_com.add(number_com)
+    return number_com
 }
